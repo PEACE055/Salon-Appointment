9bb053d52efeb37d49eab48a358f53751fb0a4ec
sujal <sujalpatil5478@gmail.com>
2025-04-12 20:13:36 +0530

    initial commit

---
+++ salonappointment/src/test/java/com/bobsalon/salonappointment/SalonappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobsalon.salonappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonappointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
